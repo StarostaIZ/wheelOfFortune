e5f2d18859426b87d0b4d737f5fe3b6b7aade841
Michał <m.wiczk@wp.pl>
2020-03-27 12:03:09 +0100

    Implement statistic view

--- frontendApp/src/app/my-profile-statistics/my-profile-statistics.component.ts
+++ frontendApp/src/app/my-profile-statistics/my-profile-statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import * as CanvasJS from './canvasjs.min';
 
 @Component({
   selector: 'app-my-profile-statistics',
@@ -6,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [
     './my-profile-statistics.component.css',
     '../../css/logoSmall.css',
+    '../../css/button.css',
   ],
 })
 export class MyProfileStatisticsComponent implements OnInit {
-
+  numberOfGames = 35;
+  wins = 25;
+  losts = 10;
+  sumOfPoints = 1500;
+  pointsAvg = 100;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // const chart = new CanvasJS.Chart('chartContainer', {
+    //   animationEnabled: true,
+    //   exportEnabled: true,
+    //   title: {
+    //     text: 'Wygrane w ciągu roku:',
+    //   },
+    //   data: [
+    //     {
+    //       type: 'column',
+    //       dataPoints: [
+    //         { y: 1, label: 'Styczeń' },
+    //         { y: 3, label: 'Luty' },
+    //         { y: 4, label: 'Marzec' },
+    //         { y: 6, label: 'Kwiecień' },
+    //         { y: 0, label: 'Maj' },
+    //         { y: 12, label: 'Czerwiec' },
+    //         { y: 3, label: 'Lipiec' },
+    //         { y: 2, label: 'Sierpień' },
+    //         { y: 1, label: 'Wrzesień' },
+    //         { y: 3, label: 'Październik' },
+    //         { y: 0, label: 'Listopad' },
+    //         { y: 0, label: 'Grudzień' },
+    //       ],
+    //     },
+    //   ],
+    // });
+    //
+    // chart.render();
+  }
 }
