a725f44d07bcc90d794b4c349e3de11c174c63b9
Maciek Bartosik <maciek@bartosik.wtf>
2020-05-04 13:41:04 +0200

    Fix update points

---
+++ backendApp/tests/Utils/Struct/FriendRequestResponseStructTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Utils\Struct;
+
+use App\Utils\Struct\FriendRequestResponseStruct;
+use PHPUnit\Framework\TestCase;
+
+class FriendRequestResponseStructTest extends TestCase
+{
+
+}
