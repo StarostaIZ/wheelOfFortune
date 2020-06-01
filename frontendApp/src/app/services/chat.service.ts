1876df9991edd2ee03d8c1db6e00b4106d751ce8
Michał <m.wiczk@wp.pl>
2020-06-01 14:37:42 +0200

    Resolve chat problem and css

--- frontendApp/src/app/services/chat.service.ts
+++ frontendApp/src/app/services/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +11,13 @@ export class ChatService {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('token')}`,
   });
+  private API_URL: string = environment.API_URL;
   constructor(private http: HttpClient, private router: Router) {}
 
   sendMessage(message, roomID) {
     const body = { message: message };
     return this.http
-      .post(`/room/${roomID}/sendMessage`, body, {
+      .post(`${this.API_URL}/room/${roomID}/sendMessage`, body, {
         headers: this.header,
       })
       .pipe();
